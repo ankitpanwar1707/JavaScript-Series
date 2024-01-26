28bd78ef0c55c4e99e2d0549ca2b895518737c68
Ankit Panwar <145194903+ankitpanwar1707@users.noreply.github.com>
2024-01-26 14:59:03 +0000

    first

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33";
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// '33' => 33
+// "33abc" => NaN
+// true => 1; false =>0;
+
+let isLogged = 1;
+let booleanIsLogged = Boolean(isLogged);
+console.log(booleanIsLogged);
+
+//1 => true ; 0=> false;
+//"" =>false;
+//"Ankit" 
