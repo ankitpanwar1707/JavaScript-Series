320796b8161cea9e60c0bc13b9e092b3c957769c
Ankit Panwar <145194903+ankitpanwar1707@users.noreply.github.com>
2024-01-26 14:38:41 +0000

    variable commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs ,not browser
+
+let name = "Ankit";
+let age = 21;
+let isLoggedIn = false;
+let state = null;
+
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof(state));
+console.log(typeof(undefined));
