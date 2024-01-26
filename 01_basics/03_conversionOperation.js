8b1e2250a0ce92f223436c49e443c3bf33b01bdc
Ankit Panwar <145194903+ankitpanwar1707@users.noreply.github.com>
2024-01-26 15:00:56 +0000

    second

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,4 +16,10 @@ console.log(booleanIsLogged);
 
 //1 => true ; 0=> false;
 //"" =>false;
-//"Ankit" 
+//"Ankit" => true;
+
+let someNumber =33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
