c30013ea39de6db1e31cd93b4c143db116b6f348
ankitpanwar1707 <ankitpanwar17072002@gmail.com>
2024-02-02 19:40:58 +0530

    object destructuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,3 +20,13 @@ const obj1 = {1:'a',2:'b'}
 const obj2= {3:'a',4:'b'}
 const obj3 = Object.assign({},obj1,obj2);
 console.log(obj3);
+
+const course ={
+    coursename:"Js in hindi",
+    price:999,
+    courseInstructor:"hitesh"
+}
+
+const {courseInstructor} = course;
+console.log(courseInstructor); 
+
